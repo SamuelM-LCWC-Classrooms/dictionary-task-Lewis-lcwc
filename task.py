987079655575ccffbb64db14cd578482ba7052b1
Lewis-lcwc <30075425@lcwc.ac.uk>
2024-09-18 10:05:15 +0000

    task2

--- task.py
+++ task.py
@@ -1,7 +1,7 @@
 def task(input): # the value of input is an integer which is the total money available
 
     result = {
-        "Needs": input*8.5, "Wants": input*0.3, "Savings": input*0.2
+        "Needs": input*0.5, "Wants": input*0.3, "Savings": input*0.2
    } # Empty dictionary
 
     # Code here...
